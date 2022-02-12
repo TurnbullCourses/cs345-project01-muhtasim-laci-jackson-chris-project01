06196dc2f3cf387ef97890948e4f7d48046da757
Utcoop <jacksonbckr13@gmail.com>
2022-02-11 21:48:22 -0500

    Implemented all unimplemented methods.

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -11,10 +11,15 @@ public class BankAccount {
     public BankAccount(String email, double startingBalance){
         if (isEmailValid(email)){
             this.email = email;
-            this.balance = startingBalance;
         }
         else {
             throw new IllegalArgumentException("Email address: " + email + " is invalid, cannot create account");
+        }
+        if(isNumberValid(startingBalance)){
+            this.balance = startingBalance;
+        }
+        else{
+            throw new IllegalArgumentException("Value: " + startingBalance + " is an invalid starting balance, cannot create account");
         }
     }
 
@@ -41,11 +46,16 @@ public class BankAccount {
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
     public void withdraw (double amount) throws InsufficientFundsException{
-        if (amount <= balance){
-            balance -= amount;
-        }
-        else {
-            throw new InsufficientFundsException("Not enough money");
+        if (isNumberValid(amount)) {    
+            if (amount <= balance){
+                balance -= amount;
+                balance = Math.round(balance * 100.0) / 100.0; // Multiply by 100 and round to cut off all 
+            }                                                  // decimals past the hundreths place. Divide by 100
+            else {                                             // to make sure the number has two decimasl again
+                throw new InsufficientFundsException("Not enough money");
+            }
+        }else{
+            throw new IllegalArgumentException("Amount to withdraw is invalid");
         }
     }
 
@@ -63,15 +73,65 @@ public class BankAccount {
         }
 
 
-    public static boolean isEmailValid(String email){
-        if (email.indexOf('@') == -1){
-            return false;
+        public static boolean isEmailValid(String email){
+            Boolean valid = true;
+            String prefix = email.split("@")[0].toString();
+    
+            if (prefix == "") {
+                return false;
+            }
+    
+            if (prefix.startsWith(".") || prefix.startsWith("!") || prefix.startsWith("#") || prefix.startsWith("'")) {
+                return false;
+            }
+    
+            if (prefix.contains("@")) {
+                return false;
+            }
+    
+            if (prefix.endsWith("-")) {
+                return false;
+            }        
+    
+            if (prefix.contains("#")) {
+                return false;
+            }
+        
+            if (email.indexOf('@') == -1){
+                return false;
+            } 
+    
+            if (email.endsWith("@")) {
+                return false;
+            }
+    
+            //check domain
+            String domain = email.split("@")[1].toString();
+    
+            if (!domain.contains(".")) {
+                return false;
+            }
+    
+            if (domain.contains("@")) {
+                return false;
+            }
+    
+            if (domain == "") {
+                return false;
+            }
+            
+            String domainLastPortion = domain.split("\\.")[1].toString();
+            if (domain.contains("#") || domain.contains("'")) {
+                return false;
+            }
+    
+            if (domainLastPortion.length() < 2) {
+                return false;
+            }
+    
+            return valid;
         }
-        else {
-            return true;
-        }
-    }
-    //test
+
     /**
      * @post checks to see if the number is valid
      */
