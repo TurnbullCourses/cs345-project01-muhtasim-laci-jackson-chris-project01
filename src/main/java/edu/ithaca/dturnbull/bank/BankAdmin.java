7f7ff3a931ad55f7e78f17321409661074e28ae9
cweil00 <cweil@ithaca.edu>
2022-02-17 15:36:41 -0500

    made getAllMoneyTest Comments

---
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAdmin.java
@@ -0,0 +1,20 @@
+package edu.ithaca.dturnbull.bank;
+
+public class BankAdmin {
+    
+    /**
+     * Gets an array of all the accounts in the Bank
+     * @return an array of all the accoutns in the bank
+     */
+    private Account[] getAllAccounts(){
+        return null;
+    }
+
+    /**
+     * Gets the total money of all the accounts in the bank
+     * @return the total money in the bank
+     */
+    public double sumAllAccounts(){
+        return 0.0;
+    }
+}
---
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAdminTest.java
@@ -0,0 +1,21 @@
+package edu.ithaca.dturnbull.bank;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class BankAdminTest {
+
+    @Test
+    void sumAllAccountsTest(){ //cannot test yet since other required classes do not exist
+        //make new admin
+        //make new teller
+        //create an account
+        //make sure ammount is zero
+        //deposit money
+        //make sure correct amount
+        //make another accoutn and deposit money
+        //make sure correct amount in bank
+    }
+    
+}
