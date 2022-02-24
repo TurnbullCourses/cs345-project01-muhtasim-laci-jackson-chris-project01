3e0f3852add48c325d39135a1973ebb060168d62
Jackson Becker <jacksonbckr13@gmail.com>
2022-02-24 15:14:31 -0500

    Implemented abstract account superclass and savings account subclass

--- src/main/java/edu/ithaca/dturnbull/bank/Account/AbstractAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Account/AbstractAccount.java
@@ -39,7 +39,7 @@ public abstract class AbstractAccount {
      * @post reduces the balance by @param amount if amount is non-negative and smaller than balance
      * increases the balance of @param transferee 
      */
-    abstract void transfer(double amoount, BankAccount transferee)  throws InsufficientFundsException;
+    abstract void transfer(double amoount, SavingsAccount transferee)  throws InsufficientFundsException;
 
     /**
      * @post checks to see if the @param email is valid
