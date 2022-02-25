c9f8c4ff7892abc90dc5bf3351eeb90df30d0b3d
cweil00 <cweil@ithaca.edu>
2022-02-25 11:37:43 -0500

    updated login test

--- src/test/java/edu/ithaca/dturnbull/bank/Bank/BankTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/Bank/BankTest.java
@@ -2,14 +2,34 @@ package edu.ithaca.dturnbull.bank.Bank;
 
 import org.junit.jupiter.api.Test;
 
+import edu.ithaca.dturnbull.bank.BankAdmin.BankAdmin;
+import edu.ithaca.dturnbull.bank.Customer.Customer;
+import edu.ithaca.dturnbull.bank.Teller.AbstractTeller;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class BankTest {
 
     @Test
-    void customersTest(){
-        
+    void loginTest(){
+        Bank bank = new Bank();
+
+        Customer customer = new Customer(0, "password");
+        bank.addCustomer(customer);
+        Customer customerTest = bank.customerLogIn(0, "password");
+        assertEquals(customer, customerTest);
+
+        AbstractTeller teller = new BankTeller(0, "password");
+        bank.addTeller(teller);
+        AbstractTeller tellerTest = bank.tellerLogIn(0, "password");
+        assertEquals(teller, tellerTest);
+
+        BankAdmin admin = new BankAdmin(0, "password");
+        bank.addAdmin(admin);
+        BankAdmin adminTest = bank.adminLogIn(0, "password");
+        assertEquals(admin, adminTest);
+
     }
     
 }
--- src/test/java/edu/ithaca/dturnbull/bank/BankAdmin/BankAdminTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAdmin/BankAdminTest.java
@@ -11,25 +11,6 @@ import edu.ithaca.dturnbull.bank.Teller.BankTeller;
 
 public class BankAdminTest {
 
-    @Test
-    void loginTest(){
-        Bank bank = new Bank();
-
-        Customer customer = new Customer(0, "password");
-        bank.addCustomer(customer);
-        Customer customerTest = bank.customerLogIn(0, "password");
-        assertEquals(customer, customerTest);
-
-        AbstractTeller teller = new BankTeller(0, "password");
-        bank.addTeller(teller);
-        AbstractTeller tellerTest = bank.tellerLogIn(0, "password");
-        assertEquals(teller, tellerTest);
-
-        BankAdmin admin = new BankAdmin(0, "password");
-        bank.addAdmin(admin);
-        BankAdmin adminTest = bank.adminLogIn(0, "password");
-        assertEquals(admin, adminTest);
-
-    }
+    
     
 }
