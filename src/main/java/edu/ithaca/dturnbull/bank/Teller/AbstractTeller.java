1b0e7940bd51aadffa6d27b9d97012cd995717fc
Muhta3 <89270569+Muhta3@users.noreply.github.com>
2022-02-24 23:00:53 -0500

    AbstractTeller fix

--- src/main/java/edu/ithaca/dturnbull/bank/Teller/AbstractTeller.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Teller/AbstractTeller.java
@@ -2,8 +2,7 @@ package edu.ithaca.dturnbull.bank.Teller;
 
 import edu.ithaca.dturnbull.bank.Customer.Customer;
 
-import java.util.Scanner;
-import java.util.Random;
+
 
 
 public abstract class AbstractTeller {
@@ -13,38 +12,10 @@ public abstract class AbstractTeller {
     protected Customer currentCustomer;
 
 
-    public boolean login(String username, String password){
-        if (this.username.equals(username)){
-            if (this.password.equals(password)){
-                return true;
-            }
-            else{
-                return false;
-            }
-        }
-        return false;
-    }
-
-
-    public int accountPin(){
-        System.out.println("Create 4 digit pin");
-        Scanner input2 = new Scanner(System.in);
-        int pin = input2.nextInt();
-        input2.close();
-        return pin;
-    }
-    
-
-    public int accountNumber(){
-        Random random = new Random();
-        int accountNum = (int)(random.nextInt()*5000);
-
-        return accountNum;
-    }
+    abstract void createAccount(Customer existCustomer, int accountType);
 
     
 
 
 
-    
 }
