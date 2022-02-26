c48a01bc953c35bc215cfb6772828067255c953e
Jackson Becker <jacksonbckr13@gmail.com>
2022-02-26 16:37:09 -0500

    Added getpassword()

--- src/main/java/edu/ithaca/dturnbull/bank/Customer/Customer.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Customer/Customer.java
@@ -2,6 +2,7 @@ package edu.ithaca.dturnbull.bank.Customer;
 
 import edu.ithaca.dturnbull.bank.Account.AbstractAccount;
 import edu.ithaca.dturnbull.bank.Account.CheckingAccount;
+import edu.ithaca.dturnbull.bank.Account.InsufficientFundsException;
 import edu.ithaca.dturnbull.bank.Account.SavingsAccount;
 
 public class Customer {
@@ -17,18 +18,6 @@ public class Customer {
         this.balance = 0.0;
         savingsAccount = null;
         checkingAccount = null;
-<<<<<<< HEAD
-=======
-
-    }
-
-    /**
-     * get total balance accross all accounts
-     * @return
-     */
-    public double getBalance(){
-        return balance;
->>>>>>> origin/bankClassBranch
     }
 
     
@@ -38,33 +27,9 @@ public class Customer {
     public int getid(){
         return customerId;
     }
-<<<<<<< HEAD
     
     public String getpassword(){
         return password;
-=======
-
-    public String getPassword(){
-        return password;
-    }
-
-    public void setCheckingsAccount(CheckingAccount checkingAccount){
-        this.checkingAccount = checkingAccount;
-        this.balance = this.balance + checkingAccount.getBalance();
-    }
-
-    public void setSavingsAccount(SavingsAccount savingsAccount){
-        this.savingsAccount = savingsAccount;
-        this.balance = this.balance + savingsAccount.getBalance();
-    }
-
-    public AbstractAccount getSavingsAccount(){
-        return savingsAccount;
-    }
-
-    public AbstractAccount getCheckingAccount(){
-        return checkingAccount;
->>>>>>> origin/bankClassBranch
     }
     
     
@@ -86,7 +51,7 @@ public class Customer {
      * @param amount is amount to dpeosit
      */
     public void depositSavingsAccount(double amount){
-        if savingsAccount != null{
+        if (savingsAccount != null){
             savingsAccount.deposit(amount);
         }
     }
@@ -95,25 +60,27 @@ public class Customer {
      * @param amount is amount to dpeosit
      */
     public void depositCheckingAccount(double amount){
-        if checkingAccount != null{
+        if (checkingAccount != null){
             checkingAccount.deposit(amount);
         }
     }
     
     /**
      * @param amount is amount to withdraw
+     * @throws InsufficientFundsException
      */
-    public void withdrawSavingsAccount(double amount){
-        if savingsAccount != null{
+    public void withdrawSavingsAccount(double amount) throws InsufficientFundsException{
+        if (savingsAccount != null){
             savingsAccount.withdraw(amount);
         }
     }
     
     /**
      * @param amount is amount to withdraw
+     * @throws InsufficientFundsException
      */
-    public void withdrawCheckingAccount(double amount){
-        if checkingAccount != null{
+    public void withdrawCheckingAccount(double amount) throws InsufficientFundsException{
+        if(checkingAccount != null){
             checkingAccount.withdraw(amount);
         }
     }
@@ -123,7 +90,7 @@ public class Customer {
      * @param customerID is the customer to transfer to
      */
     public void transferSavingsAccount(double amount, int customerID){
-        if savingsAccount != null{
+        if (savingsAccount != null){
             savingsAccount.transfer(amount, bank.getCustomers().get(id).getSavingsAccount());
         }
     }
@@ -133,7 +100,7 @@ public class Customer {
      * @param customerID is the customer to transfer to
      */
     public void transferCheckingAccount(double amount, int customerID){
-        if checkingAccount != null{
+        if (checkingAccount != null){
             checkingAccount.transfer(amount, bank.getCustomers().get(id).getCheckingAccount());
         }
     }
@@ -142,8 +109,8 @@ public class Customer {
     * @return Get the balance from the savings account
     */
     public double getSavingsBalance(){
-        if savingsAccount != null{
-            return savingsAccount.getBalance()
+        if (savingsAccount != null){
+            return savingsAccount.getBalance();
         }
     }
     
@@ -151,8 +118,8 @@ public class Customer {
     * @return Get the balance from the checking account
     */
     public double getCheckingBalance(){
-        if checkingAccount != null{
-            return checkingAccount.getBalance()
+        if (checkingAccount != null){
+            return checkingAccount.getBalance();
         }
     }    
 }
