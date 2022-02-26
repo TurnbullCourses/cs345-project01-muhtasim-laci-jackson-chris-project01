bcdd72568ea5d336c279aa1913dd7a0314d76563
Jackson <jacksonbckr13@gmail.com>
2022-02-26 15:49:39 -0500

    Implemented Customer Class methods

--- src/main/java/edu/ithaca/dturnbull/bank/Customer/Customer.java
+++ src/main/java/edu/ithaca/dturnbull/bank/Customer/Customer.java
@@ -15,24 +15,104 @@ public class Customer {
         this.balance = 0.0;
         savingsAccount = null;
         checkingAccount = null;
-
     }
 
+    /*
+    * 
+    */
     public double getBalance(){
         return balance;
     }
-
+    /*
+    * 
+    */
     public int getid(){
         return customerId;
     }
-
-    public void setCheckingsAccount(CheckingAccount checkingAccount){
+    
+    /*
+    * 
+    */
+    public void setCheckingAccount(CheckingAccount checkingAccount){
         this.checkingAccount = checkingAccount;
     }
-
+    
+    /*
+    * 
+    */
     public void setSavingsAccount(SavingsAccount savingsAccount){
         this.savingsAccount = savingsAccount;
-        
     }
     
+    /*
+    * 
+    */
+    public void depositSavingsAccount(double amount){
+        if savingsAccount != null{
+            savingsAccount.deposit(amount);
+        }
+    }
+    
+    /*
+    * 
+    */
+    public void depositCheckingAccount(double amount){
+        if checkingAccount != null{
+            checkingAccount.deposit(amount);
+        }
+    }
+    
+    /*
+    * 
+    */
+    public void withdrawSavingsAccount(double amount){
+        if savingsAccount != null{
+            savingsAccount.withdraw(amount);
+        }
+    }
+    
+    /*
+    * 
+    */
+    public void withdrawCheckingAccount(double amount){
+        if checkingAccount != null{
+            checkingAccount.withdraw(amount);
+        }
+    }
+    
+    /*
+    * 
+    */
+    public void transferSavingsAccount(double amount, int customerID){
+        if savingsAccount != null{
+            savingsAccount.transfer(amount, bank.getCustomers().get(id).getSavingsAccount());
+        }
+    }
+    
+    /*
+    * @ amount
+    */
+    public void transferCheckingAccount(double amount, int customerID){
+        if checkingAccount != null{
+            checkingAccount.transfer(amount, bank.getCustomers().get(id).getCheckingAccount());
+        }
+    }
+
+    /*
+    * Get the balance from the savings account
+    */
+    public double getSavingsBalance(){
+        if savingsAccount != null{
+            return savingsAccount.getBalance()
+        }
+    }
+    
+    /*
+    * Get the balance from the savings account
+    */
+    public double getCheckingBalance(){
+        if checkingAccount != null{
+            return checkingAccount.getBalance()
+        }
+    }    
 }
