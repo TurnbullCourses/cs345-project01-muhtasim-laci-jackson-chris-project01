30525b33d8fc7932ae604c67e6d3add8606a0992
cweil00 <cweil@ithaca.edu>
2022-02-26 12:51:18 -0500

    removed login tests

--- src/test/java/edu/ithaca/dturnbull/bank/Teller/BankTellerTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/Teller/BankTellerTest.java
@@ -8,14 +8,6 @@ import edu.ithaca.dturnbull.bank.Teller.BankTeller;
 
 public class BankTellerTest {
 
-    @Test
-    void loginTest(){
-        BankTeller bankTeller = new BankTeller();
-        bankTeller.login("MWaowski123", "Gatorade749!");
-        assertFalse(bankTeller.login("MikeW", "Powerade947!"));
-        bankTeller.createAccount(); 
-    }
-
     
     
     void createAccountTest(){
