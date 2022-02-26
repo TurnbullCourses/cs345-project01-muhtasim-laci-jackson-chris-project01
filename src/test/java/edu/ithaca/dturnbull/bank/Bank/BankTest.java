3be5d411e2e18517bb5a19516087f47dc044675d
cweil00 <cweil@ithaca.edu>
2022-02-26 13:38:30 -0500

    tests for addings accounts to the account list

--- src/test/java/edu/ithaca/dturnbull/bank/Bank/BankTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/Bank/BankTest.java
@@ -42,6 +42,8 @@ public class BankTest {
     void createNewAccountTest(){ //this is more of an integration test than a unit test
         Bank bank = new Bank();
 
+        assertEquals(0, bank.getAccounts().size()); //make sure no accounts in bank
+
         Customer customer = new Customer(0, "password");
         bank.addCustomer(customer);
 
@@ -52,18 +54,22 @@ public class BankTest {
         bank.createNewAccount(teller, customer, 0, 0, 0, 500); //make checking account
         assertEquals(500, customer.getCheckingAccount().getBalance()); // should have 500 balance in new accounts
         assertEquals(500, customer.getBalance()); //should only have 500 accross all accounts as well
+        assertEquals(1, bank.getAccounts().size()); //make sure account was added to account list
 
         assertEquals(null, customer.getSavingsAccount());
         bank.createNewAccount(teller, customer, 1, 1000, 5, 500);
         assertEquals(500, customer.getSavingsAccount().getBalance());
         assertEquals(1000, customer.getBalance());
+        assertEquals(2, bank.getAccounts().size());
 
         bank.createNewAccount(teller, 1, "password", 0, 0, 0, 200);
+        assertEquals(3, bank.getAccounts().size());
         Customer customer1 = bank.customerLogIn(1, "password");
         assertEquals(customer1.getCheckingAccount().getBalance(), 200); //make sure customer was created in right spot and has correct balance
         bank.createNewAccount(teller, 1, "password", 1, 500, 2, 200);
         assertEquals(customer1.getSavingsAccount().getBalance(), 200); //make sure that a new customer is not created and a new account is made
         assertEquals(customer1.getBalance(), 400);
+        assertEquals(4, bank.getAccounts().size());
 
     }
     
