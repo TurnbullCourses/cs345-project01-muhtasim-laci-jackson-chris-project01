5c0fa6cd71b228b8125821f8788d60e72749b1de
Laci Young <lyoung4@ithaca.edu>
2022-02-26 21:39:17 -0500

    transfer test

--- src/test/java/edu/ithaca/dturnbull/bank/Teller/BankTellerTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/Teller/BankTellerTest.java
@@ -51,5 +51,28 @@ public class BankTellerTest {
         bankAccount.withdraw(100.000);
         assertEquals(0, bankAccount.getBalance());
     }
+  
+    @Test
+    void transferTest() throws InsufficientFundsException {
+        BankTeller bankAccount = new BankTeller("a@b.com", 200);
+        BankTeller newAccount = new BankTeller("new@mail.com", 0);
+
+        bankAccount.transfer(100, newAccount);
+
+        assertEquals(100, newAccount.getBalance()); // valid transfer - assures initial account had money withdrawn
+                                                    // correctly
+        assertEquals(100, bankAccount.getBalance()); // valid transfer - assures transfer account had money added
+                                                     // correctly
+
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.transfer(500, newAccount)); // transfer amount
+                                                                                                     // is greater than
+                                                                                                     // balance - border
+                                                                                                     // case <0
+
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.transfer(5.87654, newAccount)); // transfer
+                                                                                                       // amount has
+                                                                                                  // more than 2
+                                                                                                       // decimals
+    }
 
 }
