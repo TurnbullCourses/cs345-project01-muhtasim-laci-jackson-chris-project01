94d01c2e90c71e43d2c0c2524ed1010e86995693
Laci Young <lyoung4@ithaca.edu>
2022-02-26 21:37:30 -0500

    withdraw test

--- src/test/java/edu/ithaca/dturnbull/bank/Teller/BankTellerTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/Teller/BankTellerTest.java
@@ -1,8 +1,12 @@
 package edu.ithaca.dturnbull.bank.Teller;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+
+import edu.ithaca.dturnbull.bank.Account.InsufficientFundsException;
+import edu.ithaca.dturnbull.bank.Teller.BankTeller;
 
 public class BankTellerTest {
 
@@ -15,7 +19,7 @@ public class BankTellerTest {
     }
 
     
-    
+    @Test
     void createAccountTest(){
         //Bank teller creates customers account
         
@@ -23,8 +27,29 @@ public class BankTellerTest {
         
     }
     
-    
 
-    
-    
+    @Test
+    void withdrawTest() throws InsufficientFundsException {
+        BankTeller bankAccount = new BankTeller("a@b.com", 200);
+        bankAccount.withdraw(100);
+        assertEquals(100, bankAccount.getBalance(), 0.001);
+
+        // Overdrawn
+        assertThrows(InsufficientFundsException.class, () -> bankAccount.withdraw(300));
+
+        // Negative number withdrawn
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(-100));
+
+        // Too many decimal places
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(100.999));
+        assertThrows(IllegalArgumentException.class, () -> bankAccount.withdraw(100.001));
+
+        // Balance does not change when an excepetion is thrown
+        assertEquals(100, bankAccount.getBalance());
+
+        // Hanging zeros are not accounted for
+        bankAccount.withdraw(100.000);
+        assertEquals(0, bankAccount.getBalance());
+    }
+
 }
